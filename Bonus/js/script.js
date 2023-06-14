df501a2ed7451cdabdfbbd2837239747aacf8805
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-06-14 11:42:58 +0200

    inizio lavoro bonus

---
+++ Bonus/js/script.js
@@ -0,0 +1,234 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            main_chat:0,
+            newMessage:'',
+            text_search:'',
+            
+            //  INIZIO ARRAY
+            contacts: [
+                {
+                    name: 'Michele',
+                    avatar: './img/avatar_1.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'Hai portato a spasso il cane?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Ricordati di stendere i panni',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 16:15:22',
+                            message: 'Tutto fatto!',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Fabio',
+                    avatar: './img/avatar_2.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '20/03/2020 16:30:00',
+                            message: 'Ciao come stai?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '20/03/2020 16:30:55',
+                            message: 'Bene grazie! Stasera ci vediamo?',
+                            status: 'received'
+                        },
+                        {
+                            date: '20/03/2020 16:35:00',
+                            message: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                            status: 'sent'
+                        }
+                    ],
+                },
+                {
+                    name: 'Samuele',
+                    avatar: './img/avatar_3.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '28/03/2020 10:10:40',
+                            message: 'La Marianna va in campagna',
+                            status: 'received'
+                        },
+                        {
+                            date: '28/03/2020 10:20:10',
+                            message: 'Sicuro di non aver sbagliato chat?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '28/03/2020 16:15:22',
+                            message: 'Ah scusa!',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Alessandro B.',
+                    avatar: './img/avatar_4.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'Lo sai che ha aperto una nuova pizzeria?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Si, ma preferirei andare al cinema',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Alessandro L.',
+                    avatar: './img/avatar_5.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'Ricordati di chiamare la nonna',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Va bene, stasera la sento',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Claudia',
+                    avatar: './img/avatar_5.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'Ciao Claudia, hai novità?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Non ancora',
+                            status: 'received'
+                        },
+                        {
+                            date: '10/01/2020 15:51:00',
+                            message: 'Nessuna nuova, buona nuova',
+                            status: 'sent'
+                        }
+                    ],
+                },
+                {
+                    name: 'Federico',
+                    avatar: './img/avatar_7.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'Fai gli auguri a Martina che è il suo compleanno!',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Grazie per avermelo ricordato, le scrivo subito!',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Davide',
+                    avatar: './img/avatar_8.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'Ciao, andiamo a mangiare la pizza stasera?',
+                            status: 'received'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:51:00',
+                            message: 'OK!!',
+                            status: 'received'
+                        }
+                    ],
+                } 
+            ]
+            // FINE ARRAY
+        }
+    },
+
+    methods: {
+        // funzione per richiamare la chat principale
+        bigChat(index) {
+            this.main_chat=index
+            console.log(this.main_chat)
+        },
+        // funzione per creare un nuovo messaggio con risposta automatica
+        newText(newMessage, index){
+            // controllo per non inviare un messaggio vuoto
+            if(this.newMessage !== ''){
+                let obj = {
+                    message: newMessage,
+                    status: 'sent',
+                    date: this.date()
+                }
+                this.contacts[index].messages.push(obj)
+                console.log(newMessage)   
+                this.newMessage=''
+                
+                // risposta automatica dopo 1 minuto
+                setTimeout(() => {
+                    let answer = {
+                        message: 'Klaatu barada nikto',
+                        status: 'received',
+                        date: this.date()
+                    }
+    
+                    this.contacts[index].messages.push(answer)
+                  }, "1000");
+            }else if(this.newMessage ==='') {
+            }
+        },
+        // funzione per generare la data attuale
+        date(){
+            let date= luxon.DateTime;
+            let today = date.now().setLocale('it').toLocaleString(date.DATETIME_SHORT_WITH_SECONDS);
+            console.log(today)
+            return today
+        },
+
+        search(text_search){
+            let found =text_search.toLowerCase()
+            console.log(found)
+            this.contacts.forEach(element => {
+                if (element.name.toLowerCase().includes(found)){
+                    element.visible=true
+                }
+                else {
+                    element.visible=false;
+                }  
+            });      
+        },
+
+    },
+
+
+}).mount('#app')
---
+++ Bonus/readme.md
@@ -0,0 +1,38 @@
+Problema: Boolzapp - a (not very) innovative messaging platform
+
+Ragionare una milestone alla volta, suddividendo il problema in sottosezioni più semplici.
+
+Milestone 1
+●	Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
+●	Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
+
+
+1- Realizzo la parte grafica della pagina web
+2- assegnare due classi css diverse ai messaggi a seconda se dell'untente o dell'interlocutore
+3- Collegare Vuejs al file e assicurarsi funzioni correttamente
+4- Creazione della lista dei contatti, prima statica così da avere una traccia visiva del risultato e poi in modo dinamico prendendo i dati dall'array nel file js --> usare v-for
+
+
+
+Milestone 2
+●	Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
+●	Click sul contatto mostra la conversazione del contatto cliccato
+
+1- Creo un ciclo v-for che estragga i messaggi contenuto nell'array message a sua volta contenuto dentro l'array contacts
+2- Faccio il binding di una classe per status e ci associo una condizione. ? se 0 a ricevuti assegno una classe css : altrimenti assegno un altra classe.
+3- Creo una variabile in appdata main_chat a cui associo il valore =0
+4- recupero questa variabile in una funzione per selezionare la chat corrispondente all'indice dell'array quando clicco su uno dei contatti.
+
+
+Milestone 3
+●	Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
+●	Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
+
+1-devo creare una funzione  newText che mi permetta di aggiungere un elemento all'array messages di ogni singola chat
+2- creo una variabile in appdata newMessage=''
+    2.1 utilizzo la variabile creata con un Vmodel all'interno dell'imput così da recuperare quanto scritto dall'utente
+    2.2 aggiungo @keyup.enter così da recuperare la funzione newText così da recuperare il testo quando premo invio
+3- all'interno della funzione credo un oggetto che avrà come proprieta status: 'sent' e message:newMessage
+    3.1 uso .push per inserire il nuovo messaggio all'interno dell'array
+4- all'interno della funzionenewText aggiungo un SetTimer insime ad un arrow function così da inserire una risposta automatica dopo 1 secondo (1000).
+
